4ced603bba2944d16b8695d6cd458f25cf2519b7
Jan-Frederic Kurzweil <jankurzweil@outlook.de>
2020-03-22 00:52:24 +0100

    Add files via upload

---
+++ Assets/Scripts/ImagePreviewController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagePreviewController : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("mouse over detected");
+    }
+}
---
+++ Assets/Scripts/ObjectTable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTable : MonoBehaviour
+{
+    public string text;
+    public TextMesh mech;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        mech.text = text;
+    }
+}
